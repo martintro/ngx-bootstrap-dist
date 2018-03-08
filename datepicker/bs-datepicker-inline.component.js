426f4a9198caa0aa4f9db776bd4e7da9526a97ce
Martin Troedsson <martin@insieme.se>
2018-03-08 10:42:47 +0100

    Initial commit

---
+++ datepicker/bs-datepicker-inline.component.js
@@ -0,0 +1,104 @@
+import { Directive, ElementRef, EventEmitter, Input, Output, Renderer2, ViewContainerRef } from '@angular/core';
+import { ComponentLoaderFactory } from '../component-loader/component-loader.factory';
+import { BsDatepickerInlineContainerComponent } from './themes/bs/bs-datepicker-inline-container.component';
+import 'rxjs/add/operator/filter';
+import { BsDatepickerInlineConfig } from './bs-datepicker-inline.config';
+import { BsDatepickerConfig } from './bs-datepicker.config';
+var BsDatepickerInlineDirective = (function () {
+    function BsDatepickerInlineDirective(_config, _elementRef, _renderer, _viewContainerRef, cis) {
+        this._config = _config;
+        this._elementRef = _elementRef;
+        /**
+         * Emits when datepicker value has been changed
+         */
+        this.bsValueChange = new EventEmitter();
+        this._subs = [];
+        // todo: assign only subset of fields
+        Object.assign(this, this._config);
+        this._datepicker = cis.createLoader(_elementRef, _viewContainerRef, _renderer);
+    }
+    Object.defineProperty(BsDatepickerInlineDirective.prototype, "bsValue", {
+        /**
+         * Initial value of datepicker
+         */
+        set: function (value) {
+            if (this._bsValue === value) {
+                return;
+            }
+            this._bsValue = value;
+            this.bsValueChange.emit(value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    BsDatepickerInlineDirective.prototype.ngOnInit = function () {
+        var _this = this;
+        this.setConfig();
+        this._datepickerRef = this._datepicker
+            .provide({ provide: BsDatepickerConfig, useValue: this._config })
+            .attach(BsDatepickerInlineContainerComponent)
+            .to(this._elementRef)
+            .show();
+        // if date changes from external source (model -> view)
+        this._subs.push(this.bsValueChange.subscribe(function (value) {
+            _this._datepickerRef.instance.value = value;
+        }));
+        // if date changes from picker (view -> model)
+        this._subs.push(this._datepickerRef.instance.valueChange.subscribe(function (value) {
+            _this.bsValue = value;
+        }));
+    };
+    BsDatepickerInlineDirective.prototype.ngOnChanges = function (changes) {
+        if (!this._datepickerRef || !this._datepickerRef.instance) {
+            return;
+        }
+        if (changes.minDate) {
+            this._datepickerRef.instance.minDate = this.minDate;
+        }
+        if (changes.maxDate) {
+            this._datepickerRef.instance.maxDate = this.maxDate;
+        }
+        if (changes.isDisabled) {
+            this._datepickerRef.instance.isDisabled = this.isDisabled;
+        }
+    };
+    /**
+     * Set config for datepicker
+     */
+    BsDatepickerInlineDirective.prototype.setConfig = function () {
+        this._config = Object.assign({}, this._config, this.bsConfig, {
+            value: this._bsValue,
+            isDisabled: this.isDisabled,
+            minDate: this.minDate || this.bsConfig && this.bsConfig.minDate,
+            maxDate: this.maxDate || this.bsConfig && this.bsConfig.maxDate
+        });
+    };
+    BsDatepickerInlineDirective.prototype.ngOnDestroy = function () {
+        this._datepicker.dispose();
+    };
+    BsDatepickerInlineDirective.decorators = [
+        { type: Directive, args: [{
+                    selector: 'bs-datepicker-inline',
+                    exportAs: 'bsDatepickerInline'
+                },] },
+    ];
+    /** @nocollapse */
+    BsDatepickerInlineDirective.ctorParameters = function () { return [
+        { type: BsDatepickerInlineConfig, },
+        { type: ElementRef, },
+        { type: Renderer2, },
+        { type: ViewContainerRef, },
+        { type: ComponentLoaderFactory, },
+    ]; };
+    BsDatepickerInlineDirective.propDecorators = {
+        'bsValue': [{ type: Input },],
+        'bsConfig': [{ type: Input },],
+        'isDisabled': [{ type: Input },],
+        'minDate': [{ type: Input },],
+        'maxDate': [{ type: Input },],
+        'bsValueChange': [{ type: Output },],
+    };
+    return BsDatepickerInlineDirective;
+}());
+export { BsDatepickerInlineDirective };
+//# sourceMappingURL=bs-datepicker-inline.component.js.map
---
+++ datepicker/themes/bs/bs-datepicker-inline-container.component.js
@@ -0,0 +1,43 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import { Component } from '@angular/core';
+import { BsDatepickerContainerComponent } from './bs-datepicker-container.component';
+import { BsDatepickerConfig } from '../../bs-datepicker.config';
+import { BsDatepickerActions } from '../../reducer/bs-datepicker.actions';
+import { BsDatepickerEffects } from '../../reducer/bs-datepicker.effects';
+import { BsDatepickerStore } from '../../reducer/bs-datepicker.store';
+var BsDatepickerInlineContainerComponent = (function (_super) {
+    __extends(BsDatepickerInlineContainerComponent, _super);
+    function BsDatepickerInlineContainerComponent(_config, _store, _actions, _effects) {
+        return _super.call(this, _config, _store, _actions, _effects) || this;
+    }
+    BsDatepickerInlineContainerComponent.decorators = [
+        { type: Component, args: [{
+                    selector: 'bs-datepicker-inline-container',
+                    providers: [BsDatepickerStore, BsDatepickerEffects],
+                    template: "<!-- days calendar view mode --> <div class=\"bs-datepicker\" [ngClass]=\"containerClass\" *ngIf=\"viewMode | async\"> <div class=\"bs-datepicker-container\"> <!--calendars--> <div class=\"bs-calendar-container\" [ngSwitch]=\"viewMode | async\"> <!--days calendar--> <div *ngSwitchCase=\"'day'\"> <bs-days-calendar-view *ngFor=\"let calendar of (daysCalendar | async)\" [class.bs-datepicker-multiple]=\"(daysCalendar | async)?.length > 1\" [calendar]=\"calendar\" [options]=\"options | async\" (onNavigate)=\"navigateTo($event)\" (onViewMode)=\"setViewMode($event)\" (onHover)=\"dayHoverHandler($event)\" (onSelect)=\"daySelectHandler($event)\" ></bs-days-calendar-view> </div> <!--months calendar--> <div *ngSwitchCase=\"'month'\"> <bs-month-calendar-view *ngFor=\"let calendar of (monthsCalendar | async)\" [class.bs-datepicker-multiple]=\"(daysCalendar | async)?.length > 1\" [calendar]=\"calendar\" (onNavigate)=\"navigateTo($event)\" (onViewMode)=\"setViewMode($event)\" (onHover)=\"monthHoverHandler($event)\" (onSelect)=\"monthSelectHandler($event)\" ></bs-month-calendar-view> </div> <!--years calendar--> <div *ngSwitchCase=\"'year'\"> <bs-years-calendar-view *ngFor=\"let calendar of (yearsCalendar | async)\" [class.bs-datepicker-multiple]=\"(daysCalendar | async)?.length > 1\" [calendar]=\"calendar\" (onNavigate)=\"navigateTo($event)\" (onViewMode)=\"setViewMode($event)\" (onHover)=\"yearHoverHandler($event)\" (onSelect)=\"yearSelectHandler($event)\" ></bs-years-calendar-view> </div> </div> <!--applycancel buttons--> <div class=\"bs-datepicker-buttons\" *ngIf=\"false\"> <button class=\"btn btn-success\">Apply</button> <button class=\"btn btn-default\">Cancel</button> </div> </div> <!--custom dates or date ranges picker--> <div class=\"bs-datepicker-custom-range\" *ngIf=\"false\"> <bs-custom-date-view [ranges]=\"_customRangesFish\"></bs-custom-date-view> </div> </div> ",
+                    host: {
+                        '(click)': '_stopPropagation($event)',
+                        style: 'display: inline-block;'
+                    }
+                },] },
+    ];
+    /** @nocollapse */
+    BsDatepickerInlineContainerComponent.ctorParameters = function () { return [
+        { type: BsDatepickerConfig, },
+        { type: BsDatepickerStore, },
+        { type: BsDatepickerActions, },
+        { type: BsDatepickerEffects, },
+    ]; };
+    return BsDatepickerInlineContainerComponent;
+}(BsDatepickerContainerComponent));
+export { BsDatepickerInlineContainerComponent };
+//# sourceMappingURL=bs-datepicker-inline-container.component.js.map
